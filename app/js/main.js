f525b44019cd819a8cc7e185225063c56b66632f
zuevpavel <zuevpavelw@gmail.com>
2020-08-03 17:52:23 +0300

    new section

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,13 @@
-$(function(){
-
+$(document).ready(function(){
+  $('.slider').slick({
+    infinite: true,
+    dots: true,
+    arrows: false,
+    // centerMode: true,
+    // variableWidth: true,
+  });
 
 
   
 });
+
