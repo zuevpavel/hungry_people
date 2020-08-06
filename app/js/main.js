a58557d8ddeebc8be1bebd598abc64967ffbb50f
zuevpavel <zuevpavelw@gmail.com>
2020-08-06 17:56:21 +0300

    change

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,15 @@ $(document).ready(function(){
     // centerMode: true,
     // variableWidth: true,
   });
+  $('.gallery__slider').slick({
+    infinite: true,
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    // slidesToScroll: 1
+    // centerMode: true,
+    // variableWidth: true,
+  });
 
 
   
